6635eb9d0d7a5ff0fa5a32435bcd603452412cec
Kevin Tran Huu <kevin.tranhuu@revature.com>
2022-04-18 14:48:27 -0400

    adding sidecar

--- week6/demo/src/main/java/com/revature/controllers/HelloController.java
+++ week6/demo/src/main/java/com/revature/controllers/HelloController.java
@@ -8,7 +8,10 @@ import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
+@Slf4j
 public class HelloController {
 
 	@Autowired
@@ -19,6 +22,15 @@ public class HelloController {
 		String ipAddress = InetAddress.getLocalHost().getHostAddress();
 		String port = env.getProperty("local.server.port");
 		
-		return String.format("Hello(version: 2) from %s:%s", ipAddress, port);
+		// Randomly generating logs, not great practice.
+		log.info("/hello, generating info logs");
+		if(Math.random() > .5) {
+			log.warn("warn log generated");
+			if(Math.random() > .5) {
+				log.error("error log generated");
+			}
+		}
+		
+		return String.format("Hello(version: 4) from %s:%s", ipAddress, port);
 	}
 }
